72b3583f59bfe5a40664e02c9ae337e819dc3af4
Ricardo Mora <r.morapreciado@globant.com>
2023-05-11 13:48:59 -0500

    What I saw today in the mentorship,
    A url was created to obtain information on a specific pokemon, an image type variable was also created and a log console was also created to view everything.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const URLListaDePokemones = "https://pokeapi.co/api/v2/pokemon"; // La línea de código en JavaScript define una constante llamada "URLListaDePokemones" que almacena la dirección URL de una API web que proporciona datos de Pokemones.  La URL se utiliza más tarde en la aplicación para hacer una solicitud HTTP a la API web y obtener los datos de Pokemones que se utilizarán en la aplicación.
 const nodePokemonList = "lista-pokemon"
 
+function mostrarPokemon(event) {
+    event.preventDefault();
+    let URLDePokemon = event.target.href
+    console.log(URLDePokemon);
+}
+
 function mostrarPokemones(listaDePokemones) {
     let listaPokemon = document.getElementById(nodePokemonList); // obtnemos el nodo de la lista que es ol y que tiene de indentificador lista-pokemon
     for (let i = 0; i < listaDePokemones.length; i++) { // La línea de código es una estructura de control de bucle "for" en JavaScript que se utiliza para recorrer una matriz o una lista de elementos, ejecutando una acción para cada uno de ellos.
@@ -12,6 +18,7 @@ function mostrarPokemones(listaDePokemones) {
         let li = document.createElement("li"); // Esta línea de código es una creación de elemento en JavaScript que se utiliza para crear un nuevo elemento HTML "li" (elemento de lista) en el árbol de DOM.
         a.appendChild(nombre);
         a.setAttribute("href", URLPokemon);
+        a.addEventListener("click", mostrarPokemon);
         li.appendChild(a);
         listaPokemon.appendChild(li);
     }
@@ -24,4 +31,5 @@ function obtenerListaDePokemones() {
         mostrarPokemones(data.results);
     });
 }
+
 obtenerListaDePokemones();
