b3b483179cd412f3fd08df2be4ddf2103731389f
Ricardo Mora <r.morapreciado@globant.com>
2023-05-25 05:32:32 -0500

    English name is placed
    All HTML, CSS and JavaScript commands are made and changed in English so that the code remains uniform

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
-const URLListaDePokemones = "https://pokeapi.co/api/v2/pokemon"; // La línea de código en JavaScript define una constante llamada "URLListaDePokemones" que almacena la dirección URL de una API web que proporciona datos de Pokemones.  La URL se utiliza más tarde en la aplicación para hacer una solicitud HTTP a la API web y obtener los datos de Pokemones que se utilizarán en la aplicación.
-const nodePokemonList = "lista-pokemon";
-const nodePokemonNombre = "nombre-pokemon";
-const nodePokemonImg = "imagen-pokemon";
+const URLListDePokemones = "https://pokeapi.co/api/v2/pokemon"; // La línea de código en JavaScript define una constante llamada "URLListaDePokemones" que almacena la dirección URL de una API web que proporciona datos de Pokemones.  La URL se utiliza más tarde en la aplicación para hacer una solicitud HTTP a la API web y obtener los datos de Pokemones que se utilizarán en la aplicación.
+const nodePokemonList = "list-pokemon";
+const nodePokemonName = "name-pokemon";
+const nodePokemonImg = "img-pokemon";
+const nodePokemonMoves = "moves-pokemon";
 
-function mostrarPokemon(pokemon) {
-    let nombrePokemon = document.getElementById(nodePokemonNombre);
-    let imagenPokemon = document.getElementById(nodePokemonImg);
-    nombrePokemon.innerHTML = pokemon.name;
-    imagenPokemon.src = pokemon.sprites.front_default;
+function showPokemon(pokemon) {
+    let namePokemon = document.getElementById(nodePokemonName);
+    let imgPokemon = document.getElementById(nodePokemonImg);
+    let movesPokemon = document.getElementById(nodePokemonMoves);
+    namePokemon.innerHTML = pokemon.name;
+    imgPokemon.src = pokemon.sprites.front_default;
+    movesPokemon.url = pokemon.moves.move.swords - dance;
 }
 
-function obtenerPokemon(event) {
+function obtainPokemon(event) {
     event.preventDefault();
     let URLDePokemon = event.target.href;
     console.log(URLDePokemon);
     fetch(URLDePokemon).then(function(response) {
         return response.json();
     }).then(function(data) {
-        mostrarPokemon(data);
+        showPokemon(data);
         console.log(data);
     });
 }
 
-function mostrarPokemones(listaDePokemones) {
-    let listaPokemon = document.getElementById(nodePokemonList); // obtnemos el nodo de la lista que es ol y que tiene de indentificador lista-pokemon
-    for (let i = 0; i < listaDePokemones.length; i++) { // La línea de código es una estructura de control de bucle "for" en JavaScript que se utiliza para recorrer una matriz o una lista de elementos, ejecutando una acción para cada uno de ellos.
-        let pokemon = listaDePokemones[i].name; // Sirve para extraer el nombre de cada objeto "pokemon" de una lista o matriz de objetos "listaDePokemones".
-        let URLPokemon = listaDePokemones[i].url;
-        let nombre = document.createTextNode(pokemon); // Esta línea de código sirve para una creación de nodo de texto en JavaScript que se utiliza para crear un nuevo nodo de texto en el árbol de DOM y asignarle un valor. En resumen, esta línea de código se utiliza para crear un nuevo nodo de texto en el árbol de DOM en JavaScript y asignarle un valor. 
+function showPokemones(listDePokemones) {
+    let listPokemon = document.getElementById(nodePokemonList); // obtnemos el nodo de la lista que es ol y que tiene de indentificador lista-pokemon
+    for (let i = 0; i < listDePokemones.length; i++) { // La línea de código es una estructura de control de bucle "for" en JavaScript que se utiliza para recorrer una matriz o una lista de elementos, ejecutando una acción para cada uno de ellos.
+        let pokemon = listDePokemones[i].name; // Sirve para extraer el nombre de cada objeto "pokemon" de una lista o matriz de objetos "listaDePokemones".
+        let URLPokemon = listDePokemones[i].url;
+        let name = document.createTextNode(pokemon); // Esta línea de código sirve para una creación de nodo de texto en JavaScript que se utiliza para crear un nuevo nodo de texto en el árbol de DOM y asignarle un valor. En resumen, esta línea de código se utiliza para crear un nuevo nodo de texto en el árbol de DOM en JavaScript y asignarle un valor. 
 
         let a = document.createElement("a");
         let li = document.createElement("li"); // Esta línea de código es una creación de elemento en JavaScript que se utiliza para crear un nuevo elemento HTML "li" (elemento de lista) en el árbol de DOM.
-        a.appendChild(nombre);
+        a.appendChild(name);
         a.setAttribute("href", URLPokemon);
-        a.addEventListener("click", obtenerPokemon);
+        a.addEventListener("click", obtainPokemon);
         li.appendChild(a);
-        listaPokemon.appendChild(li);
+        listPokemon.appendChild(li);
     }
 }
 
-function obtenerListaDePokemones() {
-    fetch(URLListaDePokemones).then(function(response) {
+function obtainListDePokemones() {
+    fetch(URLListDePokemones).then(function(response) {
         return response.json();
     }).then(function(data) {
-        mostrarPokemones(data.results);
+        showPokemones(data.results);
     });
 }
 
-obtenerListaDePokemones();
+obtainListDePokemones();
